8982088015fe39bb7b85bd91df3b7400f3e499fe
unknown <elliothoeg@hotmail.com>
2017-09-25 15:55:39 +0200

    Validerings Fix (la till email stöd)
    bildspel fix
    responsivitet

--- HTML PROLLE/javascripts.js
+++ HTML PROLLE/javascripts.js
@@ -43,10 +43,16 @@ $("#enamn").keyup(function(event) {
         
             var input = $(this);
             var inputVal = input.val();
-        
+            var regx = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
             if(inputVal){
                 $("#errorEmail").css("display", "none");
                 fieldsEmpty = false;
+
+                if(!regx.test(inputVal)) {
+                    $("#errorEmail").css("display", "block");
+                    $("#errorEmail").empty();
+                    $("#errorEmail").append('<li>Fältet Epost är inte i rätt format.</li>');
+                }
 
             } else {
                 $("#errorEmail").css("display", "block");
@@ -67,8 +73,6 @@ $("#enamn").keyup(function(event) {
                     fieldsEmpty = false;
 
                     if(!regx.test(inputVal)) {
-                        console.log(inputVal);
-                        console.log(regx);
                         $("#errorPhone").css("display", "block");
                         $("#errorPhone").empty();
                         $("#errorPhone").append('<li>Fältet Telefonnummer får endast innehålla siffror.</li>');
@@ -221,6 +225,7 @@ $("#submit").click(function () {
             { } $(this).toggleClass('open');
             $("#header").toggleClass('open');
             $("#headerList").toggleClass('open');
+            
         });
     });
 
